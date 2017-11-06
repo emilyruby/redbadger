2f05afa2ff52ce45b96b353208c5a702d4ccab3d
emilyruby <zcabeme@ucl.ac.uk>
2017-11-06 17:16:20 +0000

    execute the sequence of commands function

--- rovers.py
+++ rovers.py
@@ -1,5 +1,26 @@
-def execute_sequence():
+def execute_sequence(start_x, start_y, direction, sequence, width, height):
+    """
+    INPUT: Start coordinates of robot, direction facing at beginning,
+    sequence of commands, then the width and height of the grid.
 
+    OUTPUT: The final position and orientation of the robot in the format:
+
+    result = [[(3, 3), 'N']]
+    """
+
+    directions = ["N", "E", "S", "W"]
+    turn_map = {
+        "R": 1,
+        "L": -1
+        }
+    moves = {
+        "N": [0, 1],
+        "S": [0, -1],
+        "E": [1, 0],
+        "W": [-1, 0]
+    }
+
+    
 
 
 def martian_robots(data):
@@ -24,14 +45,14 @@ def martian_robots(data):
     for robot in range(1, len(data), 2):
 
         start_x, start_y, direction = data[robot].split(" ")
-        commands = data[robot + 1]
+        sequence = data[robot + 1]
 
         end_sequence.append(
             execute_commands(
                 start_x,
                 start_y,
                 direction,
-                commands,
+                sequence,
                 width,
                 height
             )
