047dc450edeb13b301e5bc27c0b46501a50f54b2
emilyruby <zcabeme@ucl.ac.uk>
2017-11-13 15:12:13 +0000

    added comments for each test

--- rovers_tests.py
+++ rovers_tests.py
@@ -31,30 +31,50 @@ class TestRobots(unittest.TestCase):
         ]
 
     def test_travelling_rover(self):
+        """
+        Test given test case for robots challenge.
+        """
+
         self.assertEqual(martian_robots(
             self.data[0]),
             '1 1 E\n3 3 N LOST\n3 3 N LOST'
         )
 
     def test_nonexistant_grid(self):
+        """
+        Test the edge case when the grid is 0 by 0 (nonexistant).
+        """
+
         self.assertEqual(martian_robots(
             self.data[1]),
             '0 0 N LOST'
         )
 
     def test_rotate_only(self):
+        """
+        Test rotation only.
+        """
+
         self.assertEqual(martian_robots(
             self.data[2]),
             '3 4 W'
         )
 
     def test_move_only(self):
+        """
+        Test movement only.
+        """
+
         self.assertEqual(martian_robots(
             self.data[3]),
             '2 0 S'
         )
 
     def test_travelling_rover2(self):
+        """
+        Test on large given grid.
+        """
+
         self.assertEqual(martian_robots(
             self.data[4]),
             '43 0 W\n13 21 E'
